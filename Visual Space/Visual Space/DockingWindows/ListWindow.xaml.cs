57a3dc49b068b9ab134aadcb7609da2647b07e91
jotasic <embedded61@gmail.com>
2017-07-31 19:16:49 +0900

    지석이와 머징

---
+++ Visual Space/Visual Space/DockingWindows/ListWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DockingLibrary;
+
+namespace Nollan.Visual_Space.DockingWindows
+{
+    /// <summary>
+    /// ListWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ListWindow : DockingLibrary.DockableContent
+    {
+        public ListWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        void OnSelectionChanged(object sender, RoutedEventArgs args)
+        {
+            string str = list.SelectedItem.ToString();
+            MessageBox.Show(str);
+        }
+
+    }
+}
--- Visual Space/Visual Space/MainWindow.xaml.cs
+++ Visual Space/Visual Space/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
-﻿#define RUN_3DVIWER_IN_UNITY_EDITER
-#define RUN_3DVIWER
+﻿// #define RUN_3DVIWER_IN_UNITY_EDITER
+// #define RUN_3DVIWER
 using Nollan.Visual_Space.Network;
 using System;
 using System.Collections.Generic;
@@ -106,6 +106,46 @@ namespace Nollan.Visual_Space
 
                             });
                             break;
+
+
+                        case WallInfo.WallInfoAction.SELECT3D:
+                            Dispatcher.Invoke(() =>
+                            {
+                                Line toSelect = null;
+
+                                // foreach 문 안에서 list의 요소를 지우면 에러가 난다.
+                                foreach (var el in mapCanvas.Children)
+                                {
+                                    if (el is Line l)
+                                    {
+                                        if (l.Name == wallInfo.Name)
+                                        {
+
+                                            toSelect = l;
+                                            break;
+                                        }
+                                    }
+                                }
+
+                                if (toSelect != null)
+                                {
+
+                                    selectedLine = toSelect;
+                                    selectedLine.Stroke = Brushes.Red;
+                                }
+
+                            });
+                            break;
+
+                        case WallInfo.WallInfoAction.DESELECT3D:
+                            Dispatcher.Invoke(() =>
+                            {
+                                if (selectedLine != null)
+                                    selectedLine.Stroke = Brushes.Black;
+                                selectedLine = null;
+
+                            });
+                            break;
                     }
                     break;
 
@@ -160,9 +200,26 @@ namespace Nollan.Visual_Space
             // 3Dviwer를 실행시킬 때만 되도록한다.
 #if !RUN_3DVIWER_IN_UNITY_EDITER && RUN_3DVIWER
             ExeViewer exeViewer = new ExeViewer();
-            mainGrid.Children.Add(exeViewer);
+            mainGrid.Children.Add(exeViewer);            
             Grid.SetRow(exeViewer, 1);
 #endif
+
+
+            //0731추가-----
+            //set window parent for dragging operations
+            ListDockManager.ParentWindow = this;
+            ExpenderDockManager.ParentWindow = this;
+
+            DockingWindows.ListWindow listWindow = new DockingWindows.ListWindow();
+            listWindow.DockManager = ListDockManager;
+            listWindow.Show(Dock.Top);
+
+
+            DockingWindows.ObjectWindow objWindowDocking = new DockingWindows.ObjectWindow();
+            objWindowDocking.DockManager = ExpenderDockManager;
+            objWindowDocking.Show(Dock.Top);
+            //-------//
+
         }
 
 
@@ -232,8 +289,30 @@ namespace Nollan.Visual_Space
 
             if (!bMouseDown && bLine_check && !linePlusMinus_Check)
             {
+                //0731추가------
+                //선이 그려져 있고, 선택된 선이 있는데 그냥 허공(캔버스)를 선택하는 경우
+                if (selectedLine != null)
+                {
+                    if (e.Source != line)
+                    {
+
+                        selectedLine.Stroke = Brushes.Black;
+                        selectedLine = null; //선택된 선 해제.
+
+                        /*
+                    1.선택된 선이 있을 때 선 그릴 경우 일단 캔버스를 클릭하게 되므로 선택해제가 됨. 
+                    2.캔버스를 누르면 선택이 취소됨
+                    3.선증가감소 체크 버튼이 켜져있을 시 선을 누르면 선택한 것임.
+                    4.선이동시(선그리기아니고, 선증감도 아닐 때) 내려놓으면 붉은색으로 남아있음.    
+
+                     */
+
+                    }
+                }
+                //-----------------//
 
 
+                
                 bMouseDown = true;
                 stPoint = e.GetPosition(null);
                 remainderToFindVertex(stPoint.X, stPoint.Y); //가장 가까운 꼭지점을 찾는다. 그 값은 튜플형식으로 Result_StartPoint에 담기게됨.
@@ -267,7 +346,7 @@ namespace Nollan.Visual_Space
                     //선택된 선을 기억
                     selectedLine = new Line();
                     selectedLine = line;
-                    stPoint = e.GetPosition(null);
+                    stPoint = stPoint = TranslatePointToCanvas(e, mapCanvas);
 
 
                     // 선 증가 감소 진입 플래그
@@ -314,7 +393,7 @@ namespace Nollan.Visual_Space
             // 선그리기 가 아닐때
             else
             {
-                curPoint = e.GetPosition(null);
+                curPoint = TranslatePointToCanvas(e, mapCanvas);
 
 
                 // 라인 증가 및 축소시킬 때.
@@ -328,7 +407,7 @@ namespace Nollan.Visual_Space
 
                     if (Flag_HorizontalLinePlusMinus != null)
                     {
-                        if (Flag_HorizontalLinePlusMinus == true) //ㅡ선(수평선) 일 때
+                        if (Flag_HorizontalLinePlusMinus == true) //ㅡ선(수평선)에서 --값으로 선이 증가할 건지 ++값으로 증가할건지(왼쪽 증가, 오른쪽증가)
                         {
                             moveLtRt_LinePlusMinus(movX);
                             writeLog(line.X1, line.Y1, line.X2, line.Y2, "마우스무브-수평선 : x1에 마우스클릭이 더 가까웠을 경우");
@@ -343,12 +422,12 @@ namespace Nollan.Visual_Space
                     if (Flag_VerticalLinePlusMinus != null)
                     {
 
-                        if (Flag_VerticalLinePlusMinus == true) //l선(수직선) 일 때. true이면 ++ 이라서 밑에쪽으로 선 증가
+                        if (Flag_VerticalLinePlusMinus == true) //l선(수직선) 일 때, --값으로 선이 증가할 건지 ++값으로 증가할건지(위쪽 증가, 밑쪽증가)
                         {
                             moveUpDw_LInePlusMinus(movY);
                             writeLog(line.X1, line.Y1, line.X2, line.Y2, "마우스무브-수직선 : y1에 마우스클릭이 더 가까웠을 경우");
                         }
-                        else // Flag_VerticalLinePlusMinus == false 수직선임. -- 하게 되면 시작점에서 위쪽으로 선 증가 
+                        else // Flag_VerticalLinePlusMinus == false 일 때(마우스 클릭이 x2에 더 가까웠을 경우)
                         {
                             moveUpDw_LInePlusMinus(movY);
                             writeLog(line.X1, line.Y1, line.X2, line.Y2, "마우스무브-수직선 : y2에 마우스클릭이 더 가까웠을 경우");
@@ -395,7 +474,7 @@ namespace Nollan.Visual_Space
 
 
 
-                curPoint = e.GetPosition(null);
+                curPoint = TranslatePointToCanvas(e, mapCanvas);
                 remainderToFindVertex(curPoint.X, curPoint.Y); //가장 가까운 꼭지점을 찾는다. 그 값은 튜플형식으로 Result_StartPoint에 담기게됨.
                 line.X2 = Result_StartPoint.Item1;
                 line.Y2 = Result_StartPoint.Item2;
@@ -501,7 +580,7 @@ namespace Nollan.Visual_Space
 #region 선 그리기 아닐 때(이동 및 축소확대)
             else // 선 그리기 아닐 때(이동 및 축소확대)
             {
-                curPoint = e.GetPosition(null);
+                curPoint = TranslatePointToCanvas(e, mapCanvas);
 
 
                 //선 축소 확대 로직
@@ -711,15 +790,7 @@ namespace Nollan.Visual_Space
 
         }
 
-        int widthCount = 0;
-        int plusCellDistance = 80;
-        private void btn_PlusWidthHeight_Click(object sender, RoutedEventArgs e)
-        {
-            widthCount++;
-            mapCanvas.Width = mapCanvas.ActualWidth + plusCellDistance;
-        }
-
-
+     
 
         public void moveParallelLIne(bool Result_separateLine, double _moveDistanceX2, double _moveDistanceY2)
         {
@@ -1127,6 +1198,13 @@ namespace Nollan.Visual_Space
         public void mapCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
         {
 
+            //처음 실행시
+            double canvasWidth = e.NewSize.Width;
+            double canvasHeight = e.NewSize.Height;
+
+            mapCanvas.Width = canvas_TotalWidth;
+            mapCanvas.Height = canvas_TotalHeight;
+
             //x축 그려질 개수 구하기
             var xlineCount = Math.Round(this.ActualWidth / _gridSize);
 
@@ -1153,6 +1231,139 @@ namespace Nollan.Visual_Space
             }
         }
 
+
+        public Point TranslatePointToCanvas(MouseButtonEventArgs e, UIElement mapCanvas) //마우스버튼다운용
+        {
+            Point _stPoint = this.mainWindow.TranslatePoint(e.GetPosition(null), mapCanvas);
+
+            return _stPoint;
+        }
+
+        public Point TranslatePointToCanvas(MouseEventArgs e, UIElement mapCanvas) //마우스 무브용 오버로딩
+        {
+            Point _stPoint = this.mainWindow.TranslatePoint(e.GetPosition(null), mapCanvas);
+
+            return _stPoint;
+        }
+
+
+
+        private void stackPanel_objectWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double stackPanelWidth = e.NewSize.Width;
+            double stackPanelHeight = e.NewSize.Height;
+
+            ExpenderDockManager.Width = stackPanelWidth;
+            ExpenderDockManager.Height = stackPanelHeight;
+            //   MessageBox.Show(string.Format("width {0}, height {1}", ExpenderDockManager.Width, ExpenderDockManager.Height));
+
+        }
+
+        private void stackPanel_listWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            double stackPanelWidth = e.NewSize.Width;
+            double stackPanelHeight = e.NewSize.Height;
+
+            ListDockManager.Width = stackPanelWidth;
+            ListDockManager.Height = stackPanelHeight;
+
+
+        }
+
+
+
+        private void ScViewer_canvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+
+            scViewer_Width = e.NewSize.Width; //현재크기값 저장 스크롤뷰어 위스
+            scViewer_Height = e.NewSize.Height;
+
+            // //리사이징 될 때 기억해놓은 액츄얼 사이즈와 변경된 e.width를 비교하여 처음 기억한 값보다 작을 때 대입해준다. 그러면 캔버스가 원래 가지고 있던 값대로 되서 스크롤바 상태가 커질 것.
+            //if (e.NewSize.Width <= canvas_TotalWidth)
+            // { 
+
+            // mapCanvas.Width = canvas_TotalWidth;
+            // mapCanvas.Height = canvas_TotalHeight;
+
+
+
+
+            //     MessageBox.Show(string.Format("width {0}, height {1}", canvas_TotalWidth, canvas_TotalHeight));
+            //     return;
+            // }
+
+            // double canvasWidth = e.NewSize.Width; //현재 스크롤뷰어의 크기만큼 대입됨.
+            // double canvasHeight = e.NewSize.Height;
+
+            // canvas_TotalWidth = canvasWidth;
+            // canvas_TotalHeight = canvasHeight;
+
+
+            // mapCanvas.Width = canvasWidth;
+            // mapCanvas.Height = canvasHeight;
+
+
+            // //리사이즈시 버튼 카운트를 초기화
+            // btn_clickWidthCount = 0;
+            // btn_clickHeightCount = 0;
+
+
+        }
+
+
+        double scViewer_Width; //스크롤뷰어 위스
+        double scViewer_Height;
+        double canvas_TotalWidth = 1400; //실행시 캔버스가 처음 가지는 넓이로 초기화
+        double canvas_TotalHeight = 800; //실행시 캔버스가 처음 가지는 높이로 초기화
+        int btn_clickWidthCount = 0;
+        int btn_clickHeightCount = 0;
+        int plusCellDistance = 80;
+
+        private void btn_PlusWidthHeight_Click(object sender, RoutedEventArgs e)
+        {
+
+            ++btn_clickWidthCount;
+            ++btn_clickHeightCount;
+
+
+            canvas_TotalWidth = mapCanvas.Width + (plusCellDistance * btn_clickWidthCount);
+            canvas_TotalHeight = mapCanvas.Height + (plusCellDistance * btn_clickHeightCount);
+
+
+            mapCanvas.Width = canvas_TotalWidth;
+            mapCanvas.Height = canvas_TotalHeight;
+
+
+        }
+
+
+        private void btn_MinusWidhtHeight_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+            if (btn_clickWidthCount < 0 && btn_clickHeightCount < 0)
+            {
+                btn_clickWidthCount = 0;
+                btn_clickHeightCount = 0;
+
+                return;
+
+            }
+
+
+            canvas_TotalWidth = mapCanvas.Width - (plusCellDistance * btn_clickWidthCount);
+            canvas_TotalHeight = mapCanvas.Height - (plusCellDistance * btn_clickHeightCount);
+
+            --btn_clickWidthCount;
+            --btn_clickHeightCount;
+
+            mapCanvas.Width = canvas_TotalWidth;
+            mapCanvas.Height = canvas_TotalHeight;
+
+        }
 
 
     }
